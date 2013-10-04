c18ebfdc10284069e3558df01b55ab559e1023aa
Paul Johnson <paul@all-the-johnsons.co.uk>
2013-10-04 12:38:21 +0100

    Modified to work with .NET

--- pullableview/pullableview/StyledPullableView.cs
+++ pullableview/pullableview/StyledPullableView.cs
@@ -1,33 +1,20 @@
 using System;
 using System.Drawing;
 using MonoTouch.UIKit;
+using MonoTouch.Foundation;
 using MonoTouch.CoreGraphics;
 
 namespace pullableview
 {
     public class StyledPullableView : PullableView
     {
-        //
-        // Prefix header for all source files of the 'PullableView' target in the 'PullableView' project
-        /**
-        
-           Subclass of PullableView that uses a background image
-        
-           @author Fabio Rodella fabio@crocodella.com.br
-        
-           */
-        /**
-        
-           @author Fabio Rodella fabio@crocodella.com.br
-        
-           */
-
-        StyledPullableView(CGRect frame)
+        [Export("initWithFrame:")]
+        public StyledPullableView(RectangleF frame)
         {
-            if ((this = base.initWithFrame(frame)))
+            if (this = base.InitWithFrame(frame))
             {
-                UIImageView imgView = new UIImageView(UIImage.ImageNamed("background.png"));
-                imgView.Frame = CGRectMake(0, 0, 320, 460);
+                UIImageView imgView = new UIImageView(UIImage.FromFile("Graphics/background.png"));
+                imgView.Frame = new RectangleF(0, 0, 320, 460);
                 this.AddSubview(imgView);
             }
 
